c39a6583a5b4a11f6fce339b3622681349b5bd06
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:59:23 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan16-Penugasan/src/if1/pkg10117125/latihan16/penugasan/IF110117125Latihan16Penugasan.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan16.penugasan;
+
+/**
+ *
+ * @author
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF 1
+ * NIM   : 10117125
+ * Deskripsi Program : 
+ */
+public class IF110117125Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= a;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= a = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+        
+    }
+    
+}
